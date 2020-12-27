a1125582265cbef69eac411e3788cf5b496a33a8
Jason Copp <jason.copp@initlive.com>
2020-12-27 15:14:44 -0500

    No code changes, format with prettier.

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,83 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 var breakpointtoggle = true;
 export function activate(context: vscode.ExtensionContext) {
 	// idea: hide the debug/toolbar ?
 
-	let debugRestart	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-restart`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.restart');
-	});
-	let debugPause	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-pause`, () => {
-		vscode.commands.executeCommand('workbench.action.debug.pause');
-	});
-	let debugStart		 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-start`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.start');
-	});
-	let debugStop		 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-stop`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.stop');
-	});
-	let debugContinue	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-continue`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.continue');
-	});
-	let debugStepInto	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-stepInto`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.stepInto');
-	});
-	let debugStepOut	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-stepOut`, () => {
-							vscode.commands.executeCommand('workbench.action.debug.stepOut');
-	});
-	let debugStepOver	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-stepOver`, () => {
-							  vscode.commands.executeCommand('workbench.action.debug.stepOver');
-	});
-	let debugToggleBreakPoints	 	= vscode.commands.registerCommand(`debug-in-titlebar.debug-toggleBreakpoints`, () => {
-		if (!breakpointtoggle) {
-			vscode.commands.executeCommand('workbench.debug.viewlet.action.enableAllBreakpoints');
+	let debugRestart = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-restart`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.restart");
 		}
-		else {
-			vscode.commands.executeCommand('workbench.debug.viewlet.action.disableAllBreakpoints');
+	);
+	let debugPause = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-pause`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.pause");
 		}
-		breakpointtoggle = !breakpointtoggle;
-	});
+	);
+	let debugStart = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-start`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.start");
+		}
+	);
+	let debugStop = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-stop`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.stop");
+		}
+	);
+	let debugContinue = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-continue`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.continue");
+		}
+	);
+	let debugStepInto = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-stepInto`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.stepInto");
+		}
+	);
+	let debugStepOut = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-stepOut`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.stepOut");
+		}
+	);
+	let debugStepOver = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-stepOver`,
+		() => {
+			vscode.commands.executeCommand("workbench.action.debug.stepOver");
+		}
+	);
+	let debugToggleBreakPoints = vscode.commands.registerCommand(
+		`debug-in-titlebar.debug-toggleBreakpoints`,
+		() => {
+			if (!breakpointtoggle) {
+				vscode.commands.executeCommand(
+					"workbench.debug.viewlet.action.enableAllBreakpoints"
+				);
+			} else {
+				vscode.commands.executeCommand(
+					"workbench.debug.viewlet.action.disableAllBreakpoints"
+				);
+			}
+			breakpointtoggle = !breakpointtoggle;
+		}
+	);
 
-	context.subscriptions.push(debugRestart,debugStart,debugStop,debugContinue,debugStepInto,debugStepOut,debugStepOver,debugPause,debugToggleBreakPoints);
+	context.subscriptions.push(
+		debugRestart,
+		debugStart,
+		debugStop,
+		debugContinue,
+		debugStepInto,
+		debugStepOut,
+		debugStepOver,
+		debugPause,
+		debugToggleBreakPoints
+	);
 }
 
-export function deactivate() {}
+export function deactivate() { }
